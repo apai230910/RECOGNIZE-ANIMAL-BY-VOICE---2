23bddc7433447d100d38f0b611f755b67ca649a3
apai230910 <91840707+apai230910@users.noreply.github.com>
2022-04-01 20:41:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification() {
+
+ navigator.mediaDevices.getUserMedia({audio : true});
+
+ classifier = ml5.soundClassifier("model.json", modelReady);
+
+}
+
+function modelReady() {
+
+ classifier.classify(gotResults);
+
+ }
